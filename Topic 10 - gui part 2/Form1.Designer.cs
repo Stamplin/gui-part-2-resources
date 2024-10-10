0903b0b18aab49e85712e88be34bc25c5af46c0d
Sakarona Bora <701017313@GoTVDSB.ca>
2024-10-10 09:40:12 -0400

    Add project files.

---
+++ Topic 10 - gui part 2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Topic_10___gui_part_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Skele = new System.Windows.Forms.Button();
+            this.Archie = new System.Windows.Forms.Button();
+            this.Knight = new System.Windows.Forms.Button();
+            this.lilPekka = new System.Windows.Forms.Button();
+            this.Giant = new System.Windows.Forms.Button();
+            this.changeBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textChange = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.changeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Clash Royal OG Deck Guide";
+            // 
+            // Skele
+            // 
+            this.Skele.Font = new System.Drawing.Font("Yu Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Skele.Location = new System.Drawing.Point(12, 88);
+            this.Skele.Name = "Skele";
+            this.Skele.Size = new System.Drawing.Size(110, 41);
+            this.Skele.TabIndex = 2;
+            this.Skele.Text = "Skele";
+            this.Skele.UseVisualStyleBackColor = true;
+            this.Skele.Click += new System.EventHandler(this.Skele_Click);
+            this.Skele.MouseHover += new System.EventHandler(this.Skele_MouseHover);
+            // 
+            // Archie
+            // 
+            this.Archie.Font = new System.Drawing.Font("Yu Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Archie.Location = new System.Drawing.Point(12, 151);
+            this.Archie.Name = "Archie";
+            this.Archie.Size = new System.Drawing.Size(110, 41);
+            this.Archie.TabIndex = 3;
+            this.Archie.Text = "Archie";
+            this.Archie.UseVisualStyleBackColor = true;
+            this.Archie.Click += new System.EventHandler(this.Archie_Click);
+            // 
+            // Knight
+            // 
+            this.Knight.Font = new System.Drawing.Font("Yu Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Knight.Location = new System.Drawing.Point(12, 210);
+            this.Knight.Name = "Knight";
+            this.Knight.Size = new System.Drawing.Size(110, 41);
+            this.Knight.TabIndex = 4;
+            this.Knight.Text = "Knight";
+            this.Knight.UseVisualStyleBackColor = true;
+            this.Knight.Click += new System.EventHandler(this.Knight_Click);
+            // 
+            // lilPekka
+            // 
+            this.lilPekka.Font = new System.Drawing.Font("Yu Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lilPekka.Location = new System.Drawing.Point(12, 272);
+            this.lilPekka.Name = "lilPekka";
+            this.lilPekka.Size = new System.Drawing.Size(110, 41);
+            this.lilPekka.TabIndex = 5;
+            this.lilPekka.Text = "lilPekka";
+            this.lilPekka.UseVisualStyleBackColor = true;
+            this.lilPekka.Click += new System.EventHandler(this.lilPekka_Click);
+            // 
+            // Giant
+            // 
+            this.Giant.Font = new System.Drawing.Font("Yu Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Giant.Location = new System.Drawing.Point(12, 340);
+            this.Giant.Name = "Giant";
+            this.Giant.Size = new System.Drawing.Size(110, 41);
+            this.Giant.TabIndex = 6;
+            this.Giant.Text = "Giant";
+            this.Giant.UseVisualStyleBackColor = true;
+            this.Giant.Click += new System.EventHandler(this.Giant_Click);
+            // 
+            // changeBox
+            // 
+            this.changeBox.Image = global::Topic_10___gui_part_2.Properties.Resources.Skellies;
+            this.changeBox.Location = new System.Drawing.Point(197, 88);
+            this.changeBox.Name = "changeBox";
+            this.changeBox.Size = new System.Drawing.Size(221, 293);
+            this.changeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.changeBox.TabIndex = 7;
+            this.changeBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Topic_10___gui_part_2.Properties.Resources.NLQOxL4;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(803, 452);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textChange
+            // 
+            this.textChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textChange.Location = new System.Drawing.Point(424, 88);
+            this.textChange.Name = "textChange";
+            this.textChange.Size = new System.Drawing.Size(352, 293);
+            this.textChange.TabIndex = 8;
+            this.textChange.Text = resources.GetString("textChange.Text");
+            this.textChange.Click += new System.EventHandler(this.textChange_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textChange);
+            this.Controls.Add(this.changeBox);
+            this.Controls.Add(this.Giant);
+            this.Controls.Add(this.lilPekka);
+            this.Controls.Add(this.Knight);
+            this.Controls.Add(this.Archie);
+            this.Controls.Add(this.Skele);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.changeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Skele;
+        private System.Windows.Forms.Button Archie;
+        private System.Windows.Forms.Button Knight;
+        private System.Windows.Forms.Button lilPekka;
+        private System.Windows.Forms.Button Giant;
+        private System.Windows.Forms.PictureBox changeBox;
+        private System.Windows.Forms.Label textChange;
+    }
+}
+
---
+++ Topic 10 - gui part 2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_10___gui_part_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Skele_Click(object sender, EventArgs e)
+        {
+            changeBox.Image = Properties.Resources.Skellies;
+            textChange.Text = "The Skeletons is a Common card that is unlocked from the Bone Pit (Arena 2). It spawns three fast, single-target, ground-targeting, melee, ground troops with very low hitpoints and very low damage. Their deployment is in a triangle formation. They are the same as those spawned by the Witch, Tombstone, Skeleton Barrel, Skeleton Army, Graveyard, and the Super Witch. A Skeletons card costs 1 Elixir to deploy.";
+        }
+
+        private void Archie_Click(object sender, EventArgs e)
+        {
+            changeBox.Image = Properties.Resources.Archers;
+            textChange.Text = "The Archers is a Common card that is unlocked from the Training Camp (Tutorial). It spawns two single-target, air-targeting, medium-ranged, ground troops with low hitpoints and low damage. Their deployment is in a horizontal formation. They wield bows, bear team-colored capes, and wear emerald green tunics. An Archers card costs 3 Elixir to deploy.";
+        }
+
+        private void Knight_Click(object sender, EventArgs e)
+        {
+            changeBox.Image = Properties.Resources.Knight;
+            textChange.Text = "The Knight is a Common card that is unlocked from the Training Camp (Tutorial). It spawns a single-target, ground-targeting. melee, ground troop with high hitpoints and moderate damage. The Knight has a blond-colored mustache and eyebrows, and wears chain mail and a chestplate along with a team-colored scarf while holding a sabre. A Knight card costs 3 Elixir to deploy.";
+        }
+
+        private void lilPekka_Click(object sender, EventArgs e)
+        {
+            changeBox.Image = Properties.Resources.MP;
+            textChange.Text = "The Mini P.E.K.K.A is a Rare card that is unlocked from the Training Camp (Tutorial). It spawns a fast, single-target, ground-targeting, melee ground troop with high hitpoints and very high damage. He is clothed in gray metal armour and wields a smaller version of the P.E.K.K.A's sword. He wears a helmet with team-colored horns and a single team-colored eye. A Mini P.E.K.K.A card costs 4 Elixir to deploy.";
+        }
+
+        private void Giant_Click(object sender, EventArgs e)
+        {
+            changeBox.Image = Properties.Resources.Giant;
+            textChange.Text = "The Giant is a Rare card that is unlocked from the Training Camp (Tutorial). It spawns a slow, single target, building-targeting, melee, ground troop with very high hitpoints and high damage. He wears a large, brown coat made of cloth and has ginger sideburns and bushy eyebrows with blue eyes. A Giant card costs 5 Elixir to deploy.";
+        }
+
+        private void textChange_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Skele_MouseHover(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
